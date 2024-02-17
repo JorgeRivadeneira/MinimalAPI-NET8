0424075160b6e4039523df1892dd260415de27fe
Jorge Rivadeneira Cevallos <jorge.rivadeneira.cevallos@gmail.com>
2024-02-17 05:48:37 -0500

    Added RepositorioPeliculas

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -17,9 +17,13 @@ namespace MinimalAPIPeliculas.Data
             modelBuilder.Entity<Genero>().Property(p => p.Nombre).HasMaxLength(50); //You can do the same using Data Annotations
             modelBuilder.Entity<Actor>().Property(x => x.Nombre).HasMaxLength(150);
             modelBuilder.Entity<Actor>().Property(x => x.Foto).IsUnicode();
+
+            modelBuilder.Entity<Pelicula>().Property(p => p.Titulo).HasMaxLength(150);
+            modelBuilder.Entity<Pelicula>().Property(p => p.Titulo).IsUnicode();
         }
         public DbSet<Genero> Generos { get; set; }
         public DbSet<Actor> Actores { get; set; }
+        public DbSet<Pelicula> Peliculas { get; set; }
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -36,6 +36,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IRepositorioGeneros, RepositorioGeneros>();
 builder.Services.AddScoped<IRepositorioActores, RepositorioActores>();
+builder.Services.AddScoped<IRepositorioPeliculas, RepositorioPeliculas>();
 builder.Services.AddScoped<IAlmacenadorArchivos, AlmacenadorArchivosLocal>();
 builder.Services.AddHttpContextAccessor(); //For storage locally
 
